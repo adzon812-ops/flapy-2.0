38d4859fd17f27c5f5e1101759d53f45f5dea97d
adzon812-ops <adzon812@gmail.com>
2026-03-02 17:36:17 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    domains: ['images.unsplash.com'],
     unoptimized: true,
+  },
+  // Оптимизация для слабого интернета
+  experimental: {
+    scrollRestoration: true,
+  },
+  // Кэширование статики
+  async headers() {
+    return [
+      {
+        source: '/:all*(svg|jpg|png)',
+        locale: false,
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=9999999999, must-revalidate',
+          }
+        ],
+      },
+    ]
   },
 }
 
