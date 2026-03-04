8d53492e674caf83b14a28cbcb7185ca43de1daf
adzon812-ops <adzon812@gmail.com>
2026-03-04 17:01:35 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -107,7 +107,7 @@ export default function HomePage() {
   const formatPrice = (p: number) => p.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ')
 
   return (
-    <div className="min-h-screen bg-gray-50 dark:bg-gray-900 pt-16 pb-20">
+    <div className="min-h-screen bg-gray-50 dark:bg-gray-900 pt-16 pb-24">
       <Header 
         search={search} 
         setSearch={setSearch}
